e34f89f5154d14046999d8f1dedc1bc91b12623c
shanpriyamsp <shanpriyamsp@gmail.com>
2025-11-10 09:48:18 +0530

    Add files via upload

---
+++ day2/Chrome.java
@@ -0,0 +1,22 @@
+package week1.day2;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float chromeVersion = 91.0f;
+		String developerName = "Google";
+		boolean isBeta = false;
+		int releaseYear = 2008;
+		char shortCutKey = 'C';
+		
+		System.out.println("----Chrome browser Info----");
+		System.out.println("chromeVersion : " + chromeVersion);
+		System.out.println("DeveloperName : " + developerName);
+		System.out.println("isBeta : " + isBeta);
+		System.out.println("releaseYear : " + releaseYear);
+		System.out.println("shortCutKey : " + shortCutKey);
+				
+	}
+
+}
---
+++ day2/FindOddNumbers.java
@@ -0,0 +1,16 @@
+package week1.day2;
+
+public class FindOddNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 10;
+		System.out.println("----Finding Odd numbers from 1 to 10-------");
+		for(int j=0; j<=i; j++) {
+			if(j%2==1) {
+				System.out.println(j);
+			}
+		}
+	}
+
+}
